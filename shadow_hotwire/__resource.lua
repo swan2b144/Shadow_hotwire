c85866bb96ae2ee35487aed7e109231ebd4cf4a1
SHADOW <49863652+swan2b144@users.noreply.github.com>
2019-08-21 20:58:54 -0600

    Add files via upload

---
+++ shadow_hotwire/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'shadows hotwiring'
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'client/main.lua',
+  'locales/en.lua',
+}
+
+server_scripts {
+  '@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+  'server/main.lua',
+  'locales/en.lua',
+}
