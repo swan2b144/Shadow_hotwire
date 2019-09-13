9ed7f8e694f7b09ab8aa5306841f83ce60485cf3
SHADOW <49863652+swan2b144@users.noreply.github.com>
2019-09-12 21:27:58 -0600

    Update esx_lockpick.sql

--- shadow_hotwire/esx_lockpick.sql
+++ shadow_hotwire/esx_lockpick.sql
@@ -1,13 +1,9 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-('lockpick', 'Lockpick', 3, 0, 1),
-('cloth', 'cloth', 3, 0, 1)
+('lockpick', 'Lockpick', 3, 0, 1)
 ;
 
 INSERT INTO `shops` (`store`, `item`, `price`) VALUES
 ('TwentyFourSeven', 'lockpick', 10000),
-('TwentyFourSeven', 'cloth', 10000),
 ('LTDgasoline', 'lockpick', 10000),
-('LTDgasoline', 'cloth', 10000),
-('RobsLiquor', 'lockpick', 10000),
-('RobsLiquor', 'cloth', 10000)
+('RobsLiquor', 'lockpick', 10000)
 ;
