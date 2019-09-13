e427cd33f0748ac0c0c7d45385af4c4bff36f396
SHADOW <49863652+swan2b144@users.noreply.github.com>
2019-09-12 21:30:52 -0600

    Update main.lua

--- shadow_hotwire/client/main.lua
+++ shadow_hotwire/client/main.lua
@@ -1,9 +1,22 @@
--------------MADE BY SHADOW
+-------------MADE BY shadow
 
 
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  }
+
+key= false
 local Radio = false 
 local PlayerData		= {}
 ESX = nil
+hotwire = false
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -21,33 +34,62 @@ AddEventHandler('esx:playerLoaded', function(xPlayer)
 	PlayerData = xPlayer
 end)
 
-function disableEngine()
-	Citizen.CreateThread(function()
-		local thief = GetPlayerPed(-1)
-		local nicecar = GetVehiclePedIsIn(thief)
-		IsVehicleNeedsToBeHotwired(nicecar)
-		SetVehicleNeedsToBeHotwired(nicecar, true)
-		SetVehicleUndriveable(nicecar, true)
-		SetVehicleEngineOn(nicecar, false, false)
-		Citizen.Wait(0)
+function disableEngine(hotwires)
+	local thief = GetPlayerPed(-1)
+	local nicecar = GetVehiclePedIsIn(thief,false)
+	Citizen.CreateThread(function() 
+		print(hotwire)
+		print(hotwires)
+		if hotwires == 5 then
+			hotwire = true
+			TriggerEvent('shadow:disable')
+		elseif hotwires == 4 then
+			hotwire = false
+			EnableEngine()
+		end
 	end)
 end
 
+
+RegisterNetEvent('shadow:disable')
+AddEventHandler('shadow:disable', function()
+	local thief = GetPlayerPed(-1)
+    local nicecar = GetVehiclePedIsIn(thief,false)
+	Citizen.CreateThread(function() 	
+		while hotwire == true do
+			Citizen.Wait(0)
+            if (IsVehicleEngineStarting(nicecar) or GetIsVehicleEngineRunning(nicecar)) then
+            	SetVehicleNeedsToBeHotwired(nicecar, true)
+				SetVehicleEngineOn(nicecar, false, false, true)
+    		end
+		end
+	end)
+end)
+
+
+
 RegisterNetEvent('shadow:EnteringVeh')
-AddEventHandler('shadow:EnteringVeh', function(veh)
-	local veh = GetVehiclePedIsTryingToEnter(GetPlayerPed(-1))
+AddEventHandler('shadow:EnteringVeh', function()
+	local thief = GetPlayerPed(-1)
+	local nicecar = GetVehiclePedIsIn(thief,false)
     Citizen.CreateThread(function() 
-        while IsVehicleNeedsToBeHotwired(veh) do
-            Citizen.Wait(0)
-            SetVehicleNeedsToBeHotwired(veh, true)
-        end
-    end)
+        while IsVehicleNeedsToBeHotwired(nicecar) do
+			Citizen.Wait(0)
+			if (IsVehicleEngineStarting(nicecar) or GetIsVehicleEngineRunning(nicecar)) then
+				SetVehicleNeedsToBeHotwired(nicecar, true)
+
+        	end
+		end
+	end)
 end)
 
 function EnableEngine()
 	Citizen.CreateThread(function()
 		local thief = GetPlayerPed(-1)
-		local nicecar = GetVehiclePedIsIn(thief)
+		local nicecar = GetVehiclePedIsIn(thief,false)
+		ClearPedTasksImmediately(theif)
+		SetVehicleRadioEnabled(nicecar, true)
+		SetVehicleNeedsToBeHotwired(nicecar, false)
 		ToggleVehicleMod(nicecar, 22, true)
 		SetVehicleAlarm(nicecar, true)
 		SetVehicleAlarm(nicecar, true)
@@ -65,20 +107,25 @@ function VehicleInFront()
 	return result
 end
 
+
 RegisterNetEvent("shadow:onUse")
 AddEventHandler("shadow:onUse", function()
 	local BitchTheif = GetPlayerPed(-1)
 	local ohlookwhatwegot = VehicleInFront()
+	local nicecar = GetVehiclePedIsIn(Bitchthief,false)
 	if GetVehiclePedIsIn(BitchTheif, false) == 0 and DoesEntityExist(ohlookwhatwegot) and IsEntityAVehicle(ohlookwhatwegot) then
 		TriggerServerEvent('shadow:removeKit')
-        SetVehicleDoorsShut(ohlookwhatwegot, true)
+		SetVehicleDoorsShut(ohlookwhatwegot, true)
+		SetVehicleNeedsToBeHotwired(ohlookwhatwegot, true)
+		SetVehicleEngineOn(ohlookwhatwegot, false, false, false)
+		
         RequestAnimDict("mini@repair")
         while not HasAnimDictLoaded("mini@repair") do
         	Citizen.Wait(100)
 		end
 		TriggerEvent("mythic_progbar:client:progress", {
-			name = "Lockpicking [stage 1]",
-			duration = 7000,
+			name = "Lockpicking",
+			duration = 15000,
 			label = "Lockpicking [stage 1]",
 			useWhileDead = false,
 			canCancel = true,
@@ -94,34 +141,31 @@ AddEventHandler("shadow:onUse", function()
 			}
 		}, function(status)
 			if not status then
+				
 				StopAnimTask(PlayerPedId(), 'mini@repair', 'fixing_a_player', 1.0)
-				TriggerEvent('shadow:EnteringVeh')
 				SetVehicleDoorsLocked(ohlookwhatwegot, false)
-				SetVehicleEngineOn(ohlookwhatwegot, false, false)
-				TaskEnterVehicle(BitchTheif, ohlookwhatwegot, 10.0, -1, 1.0, 1, 0)
+				TaskEnterVehicle(BitchTheif, ohlookwhatwegot, 10.0, -1, 1.0, 1, 0)	
 				Citizen.Wait(3500)
-				disableEngine()
+				disableEngine(5)
 				TriggerEvent('shadow:lockpick2')
 			end
 		end)
     return
-    else
-      exports['mythic_notify']:DoHudText('inform', 'You must be near a vehicle to be able to lockpick it')
+	else
+		exports.pNotify:SendNotification({text = "You must be near a vehicle to be able to lockpick it", type = "success", queue = "left", timeout = 4000, layout = "centerRight"})
     end
 end)
 
 RegisterNetEvent("shadow:lockpick2")
 AddEventHandler("shadow:lockpick2", function()
-	local BitchTheif = GetPlayerPed(-1)
-    local mycarnow = GetVehiclePedIsIn(BitchTheif, false)
     RequestAnimDict("veh@std@ds@base")
     while not HasAnimDictLoaded("veh@std@ds@base") do
         Citizen.Wait(100)
 	end
 	TriggerEvent("mythic_progbar:client:progress", {
 		name = "Lockpicking [stage 2]",
-		duration = 10000,
-		label = "Hotwiring [stage 2]",
+		duration = 20000,
+		label = "Stripping Wires",
 		useWhileDead = false,
 		canCancel = true,
 		controlDisables = {
@@ -139,6 +183,7 @@ AddEventHandler("shadow:lockpick2", function()
 			StopAnimTask(PlayerPedId(), 'veh@std@ds@base', 'hotwire', 1.0)
 			Citizen.Wait(1000)
 			TriggerEvent('shadow:lockpick3')
+		else  disableEngine(5)
 		end
 	end)
 end)
@@ -153,8 +198,8 @@ AddEventHandler("shadow:lockpick3", function()
 	end
 	TriggerEvent("mythic_progbar:client:progress", {
 		name = "Lockpicking [stage 3]",
-		duration = 5000,
-		label = "Fuses burnt out reparing fuses",
+		duration = 25000,
+		label = "Splicing Wires",
 		useWhileDead = false,
 		canCancel = true,
 		controlDisables = {
@@ -170,83 +215,15 @@ AddEventHandler("shadow:lockpick3", function()
 	}, function(status)
 		if not status then
 			SetVehicleDoorsLocked(whatthefuck, 1)
-			exports['mythic_notify']:DoHudText('success', 'Vehicle Unlocked')
+			exports.pNotify:SendNotification({text = "Vehicle Hotwired", type = "success", queue = "left", timeout = 4000, layout = "centerRight"})
+
 			Citizen.Wait(2000)
-			EnableEngine()
+			disableEngine(4)
 			if not Radio then
 				SetVehicleRadioEnabled(whatthefuck, false)
+			else disableEngine(5)
 			end
 		end
 	end)
 end)
 
-RegisterNetEvent('shadow:clean')
-AddEventHandler('shadow:clean', function()
-    local playerPed = GetPlayerPed(-1)
-    local vehicle = VehicleInFront()
-    if GetVehiclePedIsIn(playerPed, false) == 0 and DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) then
-        RequestAnimDict("amb@world_human_maid_clean")
-        while not HasAnimDictLoaded("amb@world_human_maid_clean") do
-            Citizen.Wait(100)
-		end
-		TriggerEvent("mythic_progbar:client:progress", {
-			name = "unique_action_name",
-			duration = 10000,
-			label = "Cleaning Car",
-			useWhileDead = false,
-			canCancel = true,
-			controlDisables = {
-				disableMovement = true,
-				disableCarMovement = true,
-				disableMouse = false,
-				disableCombat = true,
-			},
-			animation = {
-				animDict = "amb@world_human_maid_clean",
-				anim = "base",
-			}
-		}, function(status)
-			if not status then
-				ClearPedTasksImmediately(ped)
-				StopAnimTask(PlayerPedId(), 'amb@world_human_maid_clean', 'base', 1.0)
-				SetVehicleDirtLevel(vehicle, 0)
-				exports['mythic_notify']:DoHudText('success', 'Your vehicle has been cleaned!')
-			end
-		end)
-	else
-        exports['mythic_notify']:DoHudText('inform', 'You must be near a vehicle to be able to clean it')
-    end
-end)
-
-local isIntrunk = false
-RegisterCommand('getin', function(source, args, rawCommand)
-	local whodis = GetEntityCoords(GetPlayerPed(-1), false)
-  	local trunk = GetClosestVehicle(whodis.x, whodis.y, whodis.z, 5.0, 0, 71)
-  	if DoesEntityExist(trunk) and GetVehicleDoorLockStatus(trunk) == 1 then
-    	if not isIntrunk then
-      		AttachEntityToEntity(GetPlayerPed(-1), trunk, -1, 0.0, -2.2, 0.5, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
-      		RaiseConvertibleRoof(trunk, false)
-      		if IsEntityAttached(GetPlayerPed(-1)) then
-        		RequestAnimDict('timetable@floyd@cryingonbed@base')
-        		while not HasAnimDictLoaded('timetable@floyd@cryingonbed@base') do
-          			Citizen.Wait(1)
-        		end
-        		TaskPlayAnim(GetPlayerPed(-1), 'timetable@floyd@cryingonbed@base', 'base', 1.0, -1, -1, 1, 0, 0, 0, 0)
-      		end
-    	end
-    	isIntrunk = true
-  	end
-end)
-
-RegisterCommand('getout', function(source, args, rawCommand)
-	local whodis = GetEntityCoords(GetPlayerPed(-1), false)
-  	local trunk = GetClosestVehicle(whodis.x, whodis.y, whodis.z, 5.0, 0, 71)
-  	if DoesEntityExist(trunk) and GetVehicleDoorLockStatus(trunk) == 1 then
-    	if isIntrunk then
-      		DetachEntity(GetPlayerPed(-1), 0, true)
-      		SetEntityVisible(GetPlayerPed(-1), true)
-      		ClearPedTasksImmediately(GetPlayerPed(-1))
-    	end
-    	isInTrunk = false
-  	end
-end)
