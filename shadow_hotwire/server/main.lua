7300bd7f6225e0816a7243084c8bd67dac95e400
SHADOW <49863652+swan2b144@users.noreply.github.com>
2019-09-12 21:28:37 -0600

    Update main.lua

--- shadow_hotwire/server/main.lua
+++ shadow_hotwire/server/main.lua
@@ -1,30 +1,18 @@
 ESX = nil
+
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterUsableItem('lockpick', function(source)
+ESX.RegisterUsableItem('hotwirekit', function(source)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	TriggerClientEvent('shadow:onUse', _source)
-end)
-
-ESX.RegisterUsableItem('cloth', function(source)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	TriggerClientEvent('shadow:cloth', _source)
 end)
 
 RegisterNetEvent('shadow:removeKit')
 AddEventHandler('shadow:removeKit', function()
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	xPlayer.removeInventoryItem('lockpick', 1)
+	xPlayer.removeInventoryItem('hotwirekit', 1)
     TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'You used x1 lockpick.' })
 end)
 
-RegisterNetEvent('shadow:removecloth')
-AddEventHandler('shadow:removecloth', function()
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	xPlayer.removeInventoryItem('cloth', 1) 
-	TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'You used x1 cloth.' })
-end)
